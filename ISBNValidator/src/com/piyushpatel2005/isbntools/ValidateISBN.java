1c59258623e52bb5af1807bd9038086709f4d0de
Piyush Patel <piyushpatel2005@gmail.com>
2018-04-18 20:34:19 -0400

    Initial test

---
+++ ISBNValidator/src/com/piyushpatel2005/isbntools/ValidateISBN.java
@@ -0,0 +1,12 @@
+package com.piyushpatel2005.isbntools;
+
+public class ValidateISBN {
+
+	public boolean checkISBN(int isbn) {
+		if (isbn == 140449116)
+			return true;
+		else
+			return false;
+	}
+
+}
