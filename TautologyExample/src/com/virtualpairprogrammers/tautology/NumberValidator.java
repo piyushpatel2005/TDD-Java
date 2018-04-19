b400934d33e5f7d21241c54ff34d3aabe9ef944b
Piyush Patel <piyushpatel2005@gmail.com>
2018-04-18 23:33:13 -0400

    Correct tautology problem

--- TautologyExample/src/com/virtualpairprogrammers/tautology/NumberValidator.java
+++ TautologyExample/src/com/virtualpairprogrammers/tautology/NumberValidator.java
@@ -4,7 +4,7 @@ public class NumberValidator {
 
 	public boolean isItPrime(int number) {
 		int maxDivisor = (int)Math.sqrt(number); 
-		for(int i=2;i<maxDivisor;i++) {
+		for(int i=2;i<=maxDivisor;i++) {
 	        if(number % i==0)
 	            return false;
 	    }
